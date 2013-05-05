6bd7fcaec372b5388ff52a8dec32bad883bb7d31
Andy Davies <andy@hymaswood.co.uk>
2013-05-05 11:44:54 +0100

    Corrected folder structure etc.
    Corrected folder structure, added colour SRM images for fruit ciders,
    assigned tags to more beers

--- beer.sql
+++ beer.sql
@@ -16,27 +16,43 @@ CREATE TABLE `Beer` (
 -- Dumping data for table `Beer`
 -- 
 
-INSERT INTO `Beer` VALUES ('Coronita', 'Corona', 'Lager', '4.6 %', 1, 5, 100);
-INSERT INTO `Beer` VALUES ('Winter Zest', 'Carling', 'Low Alcohol', '2.8 %', 1, 4, 100);
-INSERT INTO `Beer` VALUES ('Guinness', 'Guinness', 'Stout', '4.1 %', 1, 23, 0);
-INSERT INTO `Beer` VALUES ('Toffee Apple Cider', 'Brothers', 'Cider', '4 %', 0, 3, 0);
-INSERT INTO `Beer` VALUES ('Carlsberg', 'Carlsberg', 'Lager', '5 %', 2, 7, 0);
-INSERT INTO `Beer` VALUES ('Original Apple Cider', 'Brothers', 'Cider', '4.7 %', 1, 3, 0);
-INSERT INTO `Beer` VALUES ('Bittersweet Apple Cider', 'Brothers', 'Cider', '5.5%', 1, 3, 100);
-INSERT INTO `Beer` VALUES ('Tutti Frutti Pear Cider', 'Brothers', 'Cider', '4 %', 1, 3, 0);
-INSERT INTO `Beer` VALUES ('Double Chocolate Stout', 'Young''s', 'Stout', '5.2 %', 0, 30, 100);
-INSERT INTO `Beer` VALUES ('Stella 4', 'Stella Artois', 'Lager', '4 %', 2, 9, 0);
-INSERT INTO `Beer` VALUES ('Stella', 'Stella Artois', 'Lager', '5.2 %', 2, 9, 0);
-INSERT INTO `Beer` VALUES ('Sole Star', 'Adnams', 'Low Alcohol Ale', '2.8%', 3, 12, 100);
-INSERT INTO `Beer` VALUES ('Summer Zest', 'Carling', 'Low Alcohol', '2.8 %', 1, 4, 100);
-INSERT INTO `Beer` VALUES ('Alcohol Free', 'Becks', 'Low Alcohol Lager', '0 %', 2, 6, 100);
-INSERT INTO `Beer` VALUES ('Farm Pressed Medium Cider', 'Perry''s', 'Cider', '5 %', 3, 9, 100);
-INSERT INTO `Beer` VALUES ('2011 Vintage Cider', 'Henry Weston', 'Cider', '8.2 %', 2, 10, 100);
-INSERT INTO `Beer` VALUES ('Crabbies', 'Crabbies', 'Ginger Beer', '4 %', 0, 5, 100);
-INSERT INTO `Beer` VALUES ('Vintage Reserve', 'Bulmers', 'Cider', '5.5 %', 1, 3, 100);
-INSERT INTO `Beer` VALUES ('Mocha Beer', 'Batemans', 'Stout', '6 %', 0, 25, 100);
-INSERT INTO `Beer` VALUES ('Pumpking', 'Wychwood Brewery', 'Seasonal Ale', '4.2 %', 2, 17, 100);
-INSERT INTO `Beer` VALUES ('Rudolph', 'White Horse', 'Seasonal Ale', '4.8 %', 2, 12, 100);
-INSERT INTO `Beer` VALUES ('Black Cherry Cider', 'Bulmers', 'Cider', '4 %', 0, 5, 100);
-INSERT INTO `Beer` VALUES ('Peach & Apricot Cider', 'Rekorderlig', 'Cider', '4 %', 0, 5, 100);
-INSERT INTO `Beer` VALUES ('Ginger Zest', 'Carling', 'Low Alcohol Lager', '2.8 %', 1, 6, 100);
+INSERT INTO `Beer` (`Beer`, `Brewery`, `Style`, `ABV`, `Hops`, `SRM`, `Live`) VALUES
+('Coronita', 'Corona', 'Mexican Lager', '4.6 %', 1, 5, 100),
+('Winter Zest', 'Carling', 'Low Alcohol Lager', '2.8 %', 1, 4, 100),
+('Guinness', 'Guinness', 'Stout', '4.1 %', 1, 23, 0),
+('Toffee Apple Cider', 'Brothers', 'Cider', '4 %', 0, 3, 0),
+('Carlsberg', 'Carlsberg', 'Lager', '5 %', 2, 7, 100),
+('Original Apple Cider', 'Brothers', 'Cider', '4.7 %', 1, 3, 0),
+('Bittersweet Apple Cider', 'Brothers', 'Cider', '5.5%', 1, 3, 100),
+('Tutti Frutti Pear Cider', 'Brothers', 'Cider', '4 %', 1, 99, 0),
+('Double Chocolate Stout', 'Young''s', 'Stout', '5.2 %', 0, 30, 100),
+('Stella 4', 'Stella Artois', 'Lager', '4 %', 2, 9, 0),
+('Stella', 'Stella Artois', 'Lager', '5.2 %', 2, 9, 0),
+('Sole Star', 'Adnams', 'Low Alcohol Ale', '2.8%', 3, 12, 100),
+('Summer Zest', 'Carling', 'Low Alcohol Lager', '2.8 %', 1, 4, 100),
+('Alcohol Free', 'Becks', 'Low Alcohol Lager', '0 %', 2, 6, 100),
+('Farm Pressed Medium Cider', 'Perry''s', 'Cider', '5 %', 3, 9, 100),
+('2011 Vintage Cider', 'Henry Weston', 'Cider', '8.2 %', 2, 10, 100),
+('Crabbies', 'Crabbies', 'Ginger Beer', '4 %', 0, 5, 100),
+('Mythos', 'Mythos', 'Lager', '4.7 %', 1, 5, 0),
+('Vintage Reserve', 'Bulmers', 'Cider', '5.5 %', 1, 3, 0),
+('Mocha Beer', 'Batemans', 'Stout', '6 %', 0, 25, 100),
+('Pumpking', 'Wychwood Brewery', 'Seasonal Ale', '4.2 %', 2, 17, 100),
+('Rudolph', 'White Horse', 'Seasonal Ale', '4.8 %', 2, 12, 100),
+('Black Cherry Cider', 'Bulmers', 'Cider', '4 %', 0, 99, 100),
+('Peach & Apricot Cider', 'Rekorderlig', 'Cider', '4 %', 0, 5, 0),
+('Ginger Zest', 'Carling', 'Low Alcohol Lager', '2.8 %', 1, 6, 100),
+('Jaipur IPA', 'Thornbridge', 'IPA', '5.9 %', 2, 15, 100),
+('Brooklyn Lager', 'Brooklyn Brewery', 'Lager', '5.2 %', 1, 20, 100),
+('Boston Lager', 'Samuel Adams', 'Lager', '4.8 %', 1, 15, 100),
+('Spindrift', 'Adnams', 'Blonde', '5 %', 1, 10, 0),
+('Carling Cider', 'Carling', 'Cider', '4.5 %', 1, 5, 0),
+('Magners', 'Magners', 'Cider', '4.5 %', 1, 5, 100),
+('Fosters', 'Fosters', 'Lager', '4 %', 1, 8, 0),
+('Corona', 'Corona', 'Mexican Lager', '4.6 %', 1, 5, 0),
+('Strawberry Cider', 'Brothers', 'Cider', '4 %', 0, 98, 0),
+('Corona Light', 'Corona', 'Mexican Lager', '3.7 %', 0, 5, 0),
+('Strawberry and Lime Cider', 'Kopparberg', 'Cider', '4 %', 0, 98, 0),
+('Pear Cider', 'Kopparberg', 'Cider', '4.5 %', 0, 4, 0),
+('Naked Apple Cider', 'Kopparberg', 'Cider', '4.5 %', 0, 4, 0),
+('Blueberry Cider', 'St. Helier', 'Cider', '5 %', 0, 97, 0);
--- sqlrfidbeerstock1.py
+++ sqlrfidbeerstock1.py
@@ -15,15 +15,279 @@ def insert_Coronita():
 	cur.execute(sql_string)
 	con.commit()
 	
-def insert_SoleStar():
+def insert_WinterZest():
 	now = datetime.datetime.now()
 	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
 	cur = con.cursor()
 	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Winter Zest","-330",now,"RFID")
 	cur.execute(sql_string)
 	con.commit()
+	
+def insert_Guinness():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Guinness","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_ToffeeAppleCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Toffee Apple Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
 
+def insert_Carlsberg():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Carlsberg","-440",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
 
+def insert_OriginalAppleCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Original Apple Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_BittersweetAppleCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Bittersweet Apple Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_TuttiFruttiPearCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Tutti Frutti Pear Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_StrawberryCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Strawberry Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_DoubleChocolateStout():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Double Chocolate Stout","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_Stella4():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Stella 4","-440",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_Stella():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Stella","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_SoleStar():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Sole Star","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_SummerZest():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Summer Zest","-330",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_AlcoholFree():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Alcohol Free","-330",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_FarmPressedMediumCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Farm Pressed Medium Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_2011VintageCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("2011 Vintage Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_Crabbies():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Crabbies","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_VintageReserve():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Vintage Reserve","-568",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_MochaBeer():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Mocha Beer","-568",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_Pumpking():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Pumpking","-568",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_Rudolph():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Rudolph","-568",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_BlackCherryCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Black Cherry Cider","-568",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+	
+def insert_GingerZest():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Ginger Zest","-330",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_PeachandApricotCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Peach & Apricot Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_Magners():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Magners","-440",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_Corona():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Corona","-330",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_CoronaLight():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Corona Light","-330",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_Fosters():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Fosters","-440",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_Spindrift():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Spindrift","-330",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_CarlingCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Carling Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()	
+
+def insert_StrawberryandLimeCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Strawberry and Lime Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_PearCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Pear Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()
+
+def insert_NakedAppleCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Naked Apple Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()	
+
+def insert_BlueberryCider():
+	now = datetime.datetime.now()
+	con=MySQLdb.connect(user="root",passwd="raspberry",db="beer")
+	cur = con.cursor()
+	sql_string = "INSERT INTO BeerStock (Beer, Quantity, Date, Reason) VALUES ('%s', '%s', '%s', '%s')" % ("Blueberry Cider","-500",now,"RFID")
+	cur.execute(sql_string)
+	con.commit()		
+	
+	
 while True:
 	message = ser.readline()
 	mes = message[0:4]
@@ -33,9 +297,93 @@ while True:
 		os.system("mpg321 beep-7.mp3")
 		print("Coronita Updated")
 	elif mes == '7087' :
-		insert_SoleStar()
+		insert_WinterZest()
 		os.system("mpg321 beep-7.mp3")
 		print("Winter Zest Updated")
+	elif mes == '2671' :
+		insert_Carlsberg()
+		os.system("mpg321 beep-7.mp3")
+		print("Carlsberg Updated")
+	elif mes == '7515' :
+		insert_Stella()
+		os.system("mpg321 beep-7.mp3")
+		print("Stella Updated")
+	elif mes == '2560' :
+		insert_SoleStar()
+		os.system("mpg321 beep-7.mp3")
+		print("Sole Star Updated")
+	elif mes == '7235' :
+		insert_SummerZest()
+		os.system("mpg321 beep-7.mp3")
+		print("Summer Zest Updated")
+	elif mes == '3502' :
+		insert_VintageReserve()
+		os.system("mpg321 beep-7.mp3")
+		print("Vintage Reserve Updated")
+	elif mes == '9100' :
+		insert_PeachandApricotCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Peach and Apricot Cider Updated")
+	elif mes == '6294' :
+		insert_GingerZest()
+		os.system("mpg321 beep-7.mp3")
+		print("Ginger Zest Updated")
+	elif mes == '9833' :
+		insert_Magners()
+		os.system("mpg321 beep-7.mp3")
+		print("Magners Updated")
+	elif mes == '2408' :
+		insert_Corona()
+		os.system("mpg321 beep-7.mp3")
+		print("Corona Updated")
+	elif mes == '2543' :
+		insert_Fosters()
+		os.system("mpg321 beep-7.mp3")
+		print("Fosters Updated")
+	elif mes == '7196' :
+		insert_Spindrift()
+		os.system("mpg321 beep-7.mp3")
+		print("Spindrift Updated")
+	elif mes == '3751' :
+		insert_CarlingCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Carling Cider Updated")
+	elif mes == '5666' :
+		insert_BittersweetAppleCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Bittersweet Apple Cider Cider Updated")
+	elif mes == '6341' :
+		insert_OriginalAppleCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Original Apple Cider Updated")
+	elif mes == '7229' :
+		insert_TuttiFruttiPearCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Tutti Frutti Cider Updated")
+	elif mes == '5162' :
+		insert_StrawberryCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Strawberry Cider Updated")
+	elif mes == '8887' :
+		insert_CoronaLight()
+		os.system("mpg321 beep-7.mp3")
+		print("Corona Light Updated")
+	elif mes == '2569' :
+		insert_StrawberryandLimeCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Strawberry and Lime Updated")
+	elif mes == '5779' :
+		insert_PearCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Pear Cider Updated")
+	elif mes == '6199' :
+		insert_NakedAppleCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Naked Apple Cider Updated")
+	elif mes == '2832' :
+		insert_BlueberryCider()
+		os.system("mpg321 beep-7.mp3")
+		print("Blueberry Cider Updated")
 	else:
 		time.sleep(0.5)
 	time.sleep(0.5)
