6c780bc291270e8d0d3609fdb9e3c2a052c7d659
Joshua Barry <joshbarry92@gmail.com>
2014-11-19 22:31:37 -0500

    Add admin pages
    add admin pages

---
+++ keg/admin.php
@@ -0,0 +1,35 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php $title = "Barry's Bar" ?>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title><?php $title ?></title>
+<link href="http://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css" />
+<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="beer.css">
+</head>
+<body>
+<h1><p align='center'><img src='BeerLabel.png' </img><br> <?php $title ?> </h1></p>
+
+<div style="text-align:center">
+<table class="center">
+  <tr>
+    <td><h2>Add a Brew</h2></td>
+    <td><h2>Manage Stock</h2></td>
+  </tr>
+</div>
+
+<!-- <form action="" method="POST">
+Brewey: <input type="text" name="brewery"><br>
+Style: <input type="text" name="style"><br>
+ABV: <input type="text" name="style"><br>
+Hops: <input type="text" name="style"><br>
+SRM: <input type="text" name="style"><br>
+<input type="submit">
+</form>
+-->
+
+
+</body>
+</html>
